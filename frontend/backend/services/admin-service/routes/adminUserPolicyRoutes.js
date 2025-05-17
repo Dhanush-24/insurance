13fd78c67af16a3fdabf2dda9f50f33c123ac3c2
k Dhanush kumar <60810043+Dhanush-24@users.noreply.github.com>
2025-05-17 19:17:19 +0530

    Delete frontend directory

--- frontend/backend/services/admin-service/routes/adminUserPolicyRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getAllUsersWithPolicies } = require("../controllers/adminUserPolicyController");
-
-router.get("/users", getAllUsersWithPolicies);
-
-module.exports = router;
--- frontend/backend/services/utils/validators.js
+++
@@ -1,3 +0,0 @@
-exports.isValidMobile = (mobile) => /^[0-9]{10}$/.test(mobile);
-exports.isValidCarNumber = (carNumber) =>
-  /^[A-Z]{2}-\d{2}-[A-Z]{2}-\d{4}$/.test(carNumber);
